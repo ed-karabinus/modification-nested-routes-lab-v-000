ea6973d5a7747449e72961178c825741ae1f3a40
ed-karabinus <ejkarabin@gmail.com>
2016-10-01 15:00:57 -0500

    Began Modifying Nested Resources Lab; 1 test not yet passing.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,8 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def edit_artist(song)
+    link_to("Edit Artist", edit_artist_song_path(song.artist, song)) unless song.artist.nil?
+  end
 end
